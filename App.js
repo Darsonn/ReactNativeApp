6af4ab4ed9973ecdfdc3e3d9c150c49faefe0e98
Darsonn <darekkoziara0@gmail.com>
2022-06-10 19:49:09 +0200

    Initial Commit

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Home' }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const HomeScreen = ({ navigation }) => {
+  return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text style={styles.text}>Witaj!</Text>
+        <Text style={styles.text}></Text>
+        <Button
+          style={styles.buttonContainer}
+          onPress={() => {
+            navigation.navigate('Profile');
+          }}
+          title="Profil"
+          color="#841584"
+        />
+        <StatusBar style="auto" />
+      </View>
+  );
+}
+
+const ProfileScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./user.png')} style={{width: 120, height: 120}} />
+      <Text></Text>
+      <Text style={styles.text}>Oto twój profil!</Text>
     </View>
   );
 }
@@ -13,8 +54,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+  buttonContainer: {
+    margin: 20,
   },
 });
